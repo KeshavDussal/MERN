ad991c2dc776bef1960b74aa0e744a870955ffcc
Keshav Dussal <ksdussal96@gmail.com>
2025-02-28 17:20:58 +0530

    Project Setup

---
+++ vms/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
