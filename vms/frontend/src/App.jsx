f4ff3acda2e6981cef44bb623fedb0bc827e331a
Keshav Dussal <ksdussal96@gmail.com>
2025-04-12 10:00:00 +0530

    Protected routes,signup and department context

--- vms/frontend/src/App.jsx
+++ vms/frontend/src/App.jsx
@@ -1,15 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DepartmentProvider } from "./context/DepartmentContext";
 import Dashboard from "./pages/Dashboard";
+import Departments from "./pages/Departments";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
-    </Routes>
+    <DepartmentProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/departments"
+            element={
+              <ProtectedRoute>
+                <Departments />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </DepartmentProvider>
   );
 };
 
