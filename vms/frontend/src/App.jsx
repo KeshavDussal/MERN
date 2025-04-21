2a682706de1b1a7056ceeaace96df5e584ae9a63
Keshav Dussal <ksdussal96@gmail.com>
2025-04-21 19:19:34 +0530

    create visitor and report context

--- vms/frontend/src/App.jsx
+++ vms/frontend/src/App.jsx
@@ -5,32 +5,54 @@ import Departments from "./pages/Departments";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import { VisitorProvider } from "./context/VisitorContext";
+import { ReportProvider } from "./context/ReportContext";
 
 const App = () => {
   return (
     <DepartmentProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/departments"
-            element={
-              <ProtectedRoute>
-                <Departments />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
+      <VisitorProvider>
+        <ReportProvider>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/departments"
+                element={
+                  <ProtectedRoute>
+                    <Departments />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/visitors"
+                element={
+                  <ProtectedRoute>
+                    <Visitors />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/reports"
+                element={
+                  <ProtectedRoute>
+                    <Reports />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </Router>
+        </ReportProvider>
+      </VisitorProvider>
     </DepartmentProvider>
   );
 };
